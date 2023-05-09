af7a91fc2c035b629d2c707590a9df3b2d354002
LiaDal <julia18g11@gmail.com>
2023-05-09 14:19:10 +0300

    fix: fix 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,11 +3,13 @@ const path = require('node:path');
 
 fs.readdir(path.join(__dirname, 'secret-folder'), (error, files) => {
   files.forEach(el => {
-    const filePath = path.resolve(__dirname, 'secret-folder', el)
+    const filePath = path.join(__dirname, 'secret-folder', el)
     const name = path.basename(filePath, path.extname(filePath))
     const extname = path.extname(filePath).slice(1)
       fs.stat(filePath, (error, stats) => {
+        if(!stats.isDirectory()) {
         console.log(`${name} - ${extname} - ${stats.size}`)
+        }
         })
    })
 })
